b7641da70a4d05749e646d0779124c1435a40d34
Bao Tran <baopei05@gmail.com>
2024-05-01 10:53:45 -0400

    updated css and form js

---
+++ customer_input.js
@@ -0,0 +1,31 @@
+const fullName = document.getElementById('fullName')
+const customerEmail = document.getElementById('email')
+const monthlyIncome = document.getElementById('monthlyIncome')
+const monthlyExpenses = document.getElementById('monthlyExpenses')
+const savingsGoal = document.getElementById('savingsGoal')
+
+const submitButton = document.getElementById('submit')
+
+const financialForm = document.getElementById('financialForm')
+
+submitButton.addEventListener('click', form_submit)
+financialForm.addEventListener('submit', form_submit)
+
+function check_input () {
+  return (
+    fullName.value &&
+    customerEmail.value &&
+    monthlyIncome.value &&
+    monthlyExpenses.value &&
+    savingsGoal.value
+  )
+}
+function form_submit (e) {
+  e.preventDefault()
+  if (!check_input()) {
+    alert('Please fill in all fields')
+    return
+  }
+  alert('Form submitted succesfully!')
+  financialForm.reset();
+}
